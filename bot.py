78e4b7c06a1266e420818ff7a91199f3cf5213ca
wiiillmatos-beep <wiiillmatos@gmail.com>
2025-11-29 15:56:35 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import os
 import asyncio
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message, FSInputFile
@@ -116,4 +117,5 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
